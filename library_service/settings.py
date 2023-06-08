d28d21cee163f89c7e9c4fc2335377e4abd9ab97
Alina Boichenko <ilub1108@gmail.com>
2023-06-08 21:53:21 +0300

    Added rest_framework to INSTALLED_APPS

--- library_service/settings.py
+++ library_service/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
     "books",
     "borrowing",
     "user"
@@ -57,7 +58,7 @@ ROOT_URLCONF = "library_service.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / 'templates']
+        "DIRS": []
         ,
         "APP_DIRS": True,
         "OPTIONS": {
