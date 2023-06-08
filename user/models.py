eabd48128b7eb6836da4970a02962c82d969ad93
Alina Boichenko <ilub1108@gmail.com>
2023-06-08 11:02:52 +0300

    Created User model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext as _
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(_("email address"),  max_length=255, unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self) -> str:
+        return self.email
+
+    class Meta:
+        ordering = ["email"]
