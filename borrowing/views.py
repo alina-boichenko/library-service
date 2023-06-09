5cd9842c3bbc92bcf83af16d3d218775e7b5927f
Alina Boichenko <ilub1108@gmail.com>
2023-06-09 22:09:11 +0300

    Implemented create method for new borrowing

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,8 +1,10 @@
-from rest_framework import mixins, viewsets
+from django.db import transaction
+from rest_framework import mixins, viewsets, status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
-from books.permissions import IsAdminOrReadOnly
+from books.models import Book
 from borrowing.models import Borrowing
 from borrowing.serializers import BorrowingListSerializer, CreateBorrowingSerializer, BorrowingDetailSerializer
 
@@ -15,7 +17,7 @@ class BorrowingViewSet(
 ):
     queryset = Borrowing.objects.all()
     authentication_classes = (JWTAuthentication,)
-    permission_classes = (IsAuthenticated, IsAdminOrReadOnly)
+    permission_classes = (IsAuthenticated,)
     serializer_class = CreateBorrowingSerializer
 
     def get_queryset(self):
@@ -33,3 +35,20 @@ class BorrowingViewSet(
             return BorrowingDetailSerializer
 
         return CreateBorrowingSerializer
+
+    @transaction.atomic
+    def create(self, request, *args, **kwargs):
+        user_id = self.request.user.id
+        book_id = request.data["book"]
+        expected_return_date = request.data["expected_return_date"]
+        borrowed_book = Book.objects.get(pk=book_id)
+
+        if borrowed_book.inventory != 0:
+            serializer = self.get_serializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            Borrowing.objects.create(user_id=user_id, book_id=book_id, expected_return_date=expected_return_date)
+            borrowed_book.inventory -= 1
+            borrowed_book.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response({"detail": "No books available"}, status=status.HTTP_400_BAD_REQUEST)
