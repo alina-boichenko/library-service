dbab01a8532d3ae9c0d8f4a6e9314d93e8f95f0e
Alina Boichenko <ilub1108@gmail.com>
2023-06-09 19:35:04 +0300

    Implemented list & detail BorrowingSerializer

---
+++ borrowing/serializers.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers
+
+from borrowing.models import Borrowing
+
+
+class BorrowingListSerializer(serializers.ModelSerializer):
+    book = serializers.CharField(source="book.title", read_only=True)
+
+    class Meta:
+        model = Borrowing
+        fields = (
+            "id",
+            "borrow_date",
+            "expected_return_date",
+            "actual_return_date",
+            "book",
+        )
+
+
+class BorrowingDetailSerializer(BorrowingListSerializer):
+    user = serializers.CharField(source="user.full_name", read_only=True)
+
+    class Meta:
+        model = Borrowing
+        fields = (
+            "id",
+            "borrow_date",
+            "expected_return_date",
+            "actual_return_date",
+            "book",
+            "user"
+        )
