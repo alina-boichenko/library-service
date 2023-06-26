a4bbd576134927cc42215ede275a954e381f72b1
Alina Boichenko <ilub1108@gmail.com>
2023-06-26 22:26:23 +0300

    Update telegram bot

--- borrowing/management/commands/bot.py
+++ borrowing/management/commands/bot.py
@@ -1,103 +1,109 @@
-import datetime
-import os
-import sys
-
-from telebot import TeleBot, types
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.dispatcher import FSMContext
+from asgiref.sync import sync_to_async
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from django.conf import settings
-from borrowing.models import Borrowing
+
+from borrowing.management.commands.keyboard import inline_keyboard
+from borrowing.management.commands.sqlite_db import (
+    get_active_borrowing,
+    get_overdue_borrowing
+)
+from borrowing.management.commands.states import LibraryStates
 from user.models import User
-import django
-
-sys.path.append("library_service/")
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'library_service.settings')
-django.setup()
 
 TELEGRAM_BOT_TOKEN = settings.TELEGRAM_BOT_TOKEN
-bot = TeleBot(token=TELEGRAM_BOT_TOKEN)
+bot = Bot(token=TELEGRAM_BOT_TOKEN)
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
 
 
-@bot.message_handler(commands=["start"])
-@bot.message_handler(content_types=["text"])
-def get_email(message):
-    telegram_id = message.chat.id
-    if User.objects.get(telegram_id=telegram_id):
-        message_reply(message)
-    else:
-        bot.send_message(
-            message.chat.id,
+@dp.message_handler(commands=["start", "help", "email"], state="*")
+async def start(message: types.Message):
+    await LibraryStates.START.set()
+    if message.text == "/start":
+        await bot.send_message(
+            message.from_user.id,
             "Hello! Please enter your email for authentication."
         )
-        bot.register_next_step_handler(message, choice_button)
+    elif message.text == "/help":
+        await bot.send_message(
+            message.from_user.id,
+            "You can use the following commands in @li_bra_ry_bot:\n"
+            "/start - to restart the bot \n"
+            "/help - for instructions \n"
+            "/library - view active or overdue borrowing"
+        )
+    else:
+        await bot.send_message(
+            message.from_user.id,
+            "Please enter your email again."
+        )
 
 
-@bot.message_handler(content_types=["text"])
-def choice_button(message):
+@dp.message_handler(state=LibraryStates.START)
+async def process_email(message: types.Message, state: FSMContext):
     email = message.text
-    if User.objects.filter(email=email):
-        user = User.objects.get(email=email)
-        user.telegram_id = message.chat.id
-        user.save()
+    await state.update_data(email=email)
 
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        active_borrowing = types.KeyboardButton("My active borrowing")
-        overdue = types.KeyboardButton("Borrowing are overdue")
-        markup.row(active_borrowing, overdue)
-
-        bot.send_message(
-            message.chat.id,
-            "You you are authenticated! Choose the next action:",
-            reply_markup=markup
+    if message.text == "/library":
+        await bot.send_message(
+            message.from_user.id,
+            "What would you like to know? "
+            "Choose a command!",
+            reply_markup=inline_keyboard
         )
-        bot.register_next_step_handler(message, message_reply)
+    else:
+
+        try:
+            user = await sync_to_async(User.objects.get)(email=email)
+            await sync_to_async(User.objects.get)(email=email)
+            user.telegram_id = message.from_user.id
+            await sync_to_async(user.save)()
+            await bot.send_message(
+                message.from_user.id,
+                "You are registered! What would you like to know? "
+                "Choose a command!",
+                reply_markup=inline_keyboard
+            )
+        except User.DoesNotExist:
+            await bot.send_message(
+                message.chat.id,
+                "Email entered incorrectly. "
+                "Please enter the command /email and try again."
+            )
+
+
+@dp.callback_query_handler(
+    lambda c: c.data in ["active", "overdue"], state="*"
+)
+async def process_command(callback_query: types.CallbackQuery):
+    await bot.answer_callback_query(callback_query.id)
+    command = callback_query.data
+    telegram_id = callback_query.from_user.id
+    user = await sync_to_async(User.objects.get)(telegram_id=telegram_id)
+
+    if command == "active":
+        result = await get_active_borrowing(user.id)
+        await bot.send_message(telegram_id, result)
+
+    elif command == "overdue":
+        result = await get_overdue_borrowing(user.id)
+        await bot.send_message(telegram_id, result)
 
     else:
-        bot.send_message(
-            message.chat.id,
-            "Email entered incorrectly. Use the /start command to try again."
+        await bot.send_message(
+            telegram_id, "I don't understand you. Choose correct command."
         )
 
 
-@bot.message_handler(content_types=["text"])
-def message_reply(message):
-    user = User.objects.get(telegram_id=message.chat.id)
-
-    if message.text == "My active borrowing":
-        result = Borrowing.objects.filter(
-            user=user.id, actual_return_date__isnull=True
-        )
-
-        if result:
-            for borrowing in result:
-                bot.send_message(message.chat.id, borrowing)
-
-        else:
-            bot.send_message(message.chat.id, "You have no active borrowing.")
-
-    if message.text == "Borrowing are overdue":
-        today = datetime.datetime.now()
-        result = Borrowing.objects.filter(
-            user=user.id,
-            actual_return_date__isnull=True,
-            expected_return_date__lt=today
-        )
-
-        if result:
-            for borrowing in result:
-                bot.send_message(message.chat.id, borrowing)
-
-        else:
-            bot.send_message(message.chat.id, "You have no overdue borrowing.")
-
-    else:
-        bot.send_message(
-            message.chat.id, "I don't understand you. Choose correct command."
-        )
+async def send_payment_confirmation(telegram_id, book_title, money):
+    await bot.send_message(
+        telegram_id,
+        f"You just successfully paid borrowing book {book_title}."
+        f" Paid {money}usd."
+    )
 
 
-def send_payment_confirmation(chat_id):
-    message = "Your payment has been received!"
-    bot.send_message(chat_id=chat_id, text=message)
-
-
-bot.infinity_polling()
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
